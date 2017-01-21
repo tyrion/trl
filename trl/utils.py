13221ccf453d038b4326ee1e4727ded0b7ae661d
Germano Gabbianelli <tyrion.mx@gmail.com>
2017-01-21 01:22:20 +0100

    make_grid works with multi dimensional arrays.

--- trl/utils.py
+++ trl/utils.py
@@ -2,8 +2,16 @@ import numpy as np
 
 
 def make_grid(x, y):
-    m = np.meshgrid(x, y, copy=False, indexing='ij')
-    return np.vstack(m).reshape(2, -1).T
+    nx = len(x)
+    ny = len(y)
+
+    # ensure 2D
+    x = x.reshape(nx, -1)
+    y = y.reshape(ny, -1)
+
+    x = np.tile(x, (1, ny)).reshape(-1, x.shape[1])
+    y = np.tile(y, (nx, 1))
+    return np.concatenate((x, y), axis=-1)
 
 
 def rec_to_array(recarray):
