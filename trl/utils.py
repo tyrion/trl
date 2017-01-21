66a0297cddac9817897f196aa4dd432a84f4b778
Germano Gabbianelli <tyrion.mx@gmail.com>
2017-01-21 20:06:41 +0100

    Add discretize_space and get_space_dim to utils.

--- trl/utils.py
+++ trl/utils.py
@@ -1,7 +1,10 @@
 import numpy as np
+import gym
+
+from gym import spaces
 
 
-def make_grid(x, y):
+def make_grid(x: np.ndarray, y: np.ndarray) -> np.ndarray:
     nx = len(x)
     ny = len(y)
 
@@ -14,7 +17,28 @@ def make_grid(x, y):
     return np.concatenate((x, y), axis=-1)
 
 
-def rec_to_array(recarray):
+def rec_to_array(recarray: np.rec.array) -> np.ndarray:
     nrows = len(recarray)
     d = recarray.view(float, np.ndarray)
     return d.reshape((nrows, len(d) // nrows))
+
+
+def discretize_space(space: gym.Space, max=20):
+    if isinstance(space, spaces.Discrete):
+        return np.arange(space.n)
+
+    if isinstance(space, spaces.Box):
+        # only 1D Box supported
+        return np.linspace(space.low, space.high, max)
+
+    # ifqi's DiscreteValued
+    try:
+        return space.values
+    except AttributeError:
+        pass
+
+    raise NotImplementedError
+
+
+def get_space_dim(space: gym.Space):
+    return np.prod(getattr(space, 'shape', 1))
