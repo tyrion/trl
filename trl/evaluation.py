b2d4b621b0e7641c02d9225f4aa67710f8d84334
Germano Gabbianelli <tyrion.mx@gmail.com>
2017-01-21 01:30:04 +0100

    Replace collect_episode(s) with interact function.
    It is able to:
     * collect episodes
     * compute metrics
     * render the environment

--- trl/evaluation.py
+++ trl/evaluation.py
@@ -1,7 +1,13 @@
+import logging
+import time
+
 import numpy as np
 from ifqi.envs import get_space_info
 
 from . import utils
+
+
+logger = logging.getLogger(__name__)
 
 
 class QPolicy:
@@ -15,17 +21,17 @@ class QPolicy:
 
 
 class RandomPolicy:
-    def __init__(self, mdp):
-        self.mdp = mdp
+    def __init__(self, env):
+        self.env = env
 
     def draw_action(self, *args):
-        return self.mdp.action_space.sample()
+        return self.env.action_space.sample()
 
 
-def allocate_episodes(mdp, n=1):
-    state_dim, action_dim, reward_dim = get_space_info(mdp)
+def allocate_dataset(env, n=1):
+    state_dim, action_dim, reward_dim = get_space_info(env)
 
-    episodes = np.zeros(mdp.horizon * n, [
+    dataset = np.zeros(n, [
         ('state', float, state_dim),
         ('action', float, action_dim),
         ('reward', float, reward_dim),
@@ -33,40 +39,62 @@ def allocate_episodes(mdp, n=1):
         ('absorbing', float, 1),
         ('done', float, 1),
     ])
-    episodes = np.rec.array(episodes, copy=False)
-    return episodes
+    dataset = np.rec.array(dataset, copy=False)
+    return dataset
 
 
-def collect_episodes(mdp, policy=None, n=1):
-    episodes = allocate_episodes(mdp, n)
+def interact(env, n=1, horizon=100, policy=None, collect=False,
+                     metrics=(), render=False):
+    if policy is None:
+        policy = RandomPolicy(env)
+
+    fps = env.metadata.get('video.frames_per_second') or 100
+    dataset = allocate_dataset(env, horizon * (n if collect else 1))
+
+    info = None
+    if metrics:
+        info = np.recarray((n,),
+                [('time', int)] + [(m.__name__, float) for m in metrics])
 
     i = 0
-    for _ in range(n):
-        e = collect_episode(mdp, policy, episodes[i:i+mdp.horizon])
-        i += e.size
+    for e in range(n):
+        state = env.reset()
+        episode = dataset[i:i+horizon] if collect else dataset
 
-    return episodes[:i]
+        for t in range(horizon):
+            if render:
+                env.render()
+                time.sleep(1 / fps)
+
+            action = policy.draw_action(state)
+            next_state, reward, done, _ = env.step(action)
+            episode[t] = (state, action, reward, next_state, 0, done)
+            logging.debug(episode[t])
+
+            if done:
+                episode = episode[:t+1]
+                break
+            state = next_state
+        episode[t].absorbing = True
+
+        t += 1
+        i += t
+
+        logging.info('Episode %d finished in %d steps.', e, t)
+        if metrics:
+            m = tuple(m(episode) for m in metrics)
+            info[e] = (t,) + m
+            logging.info('Metrics: %s', m)
+
+    return dataset if collect else None, info
 
 
-def collect_episode(mdp, policy=None, episodes=None):
-    if policy is None:
-        policy = RandomPolicy(mdp)
 
-    if episodes is None:
-        episodes = allocate_episodes(mdp, 1)
+def discounted(gamma=0.9):
+    f = lambda e: sum(e.reward * np.power(gamma, np.arange(len(e))))
+    f.__name__ = 'discounted'
+    return f
 
-    state = mdp.reset()
+def average(e):
+    return e.reward.mean()
 
-    for t in range(mdp.horizon):
-        action = policy.draw_action(state)
-        next_state, reward, done, _ = mdp.step(action)
-        episodes[t] = (state, action, reward, next_state, 0, done)
-
-        if done:
-            episodes = episodes[:t+1]
-            break
-        state = next_state
-    episodes[t].absorbing = True
-    episodes.flags['WRITEABLE'] = False
-
-    return episodes
