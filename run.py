5e2306f34aaca10a4b45ec930f7dbabb3f817d12
Germano Gabbianelli <tyrion.mx@gmail.com>
2017-01-23 14:02:01 +0100

    Improve logging of run. Add ExtraTrees to it.

--- run.py
+++ run.py
@@ -9,6 +9,7 @@ import numpy as np
 
 
 from scipy.optimize import curve_fit
+from sklearn.ensemble import ExtraTreesRegressor
 
 from trl import algorithms, ifqi, regressor
 from trl.experiment import Experiment
@@ -25,6 +26,11 @@ class CurveFitQRegressor(regressor.Regressor):
 
     def predict(self, x):
         return self.Q(x, *self.params)
+
+
+class ExtraTreesRegressor(ExtraTreesRegressor, regressor.SkLearnRegressorMixin):
+    pass
+
 
 
 def build_nn(activation='sigmoid', input_dim=2, output_dim=2):
@@ -56,8 +62,14 @@ def build_nn2(activation='sigmoid', input_dim=2, output_dim=2):
     return regressor.KerasRegressor(model, input_dim, callbacks=[cb],
                                     nb_epoch=50, batch_size=100)
 
+
 def build_curve_fit(input_dim=2, output_dim=1):
     return CurveFitQRegressor(np.array([0,0]))
+
+
+def build_extra_trees(input_dim=2, output_dim=1):
+    return ExtraTreesRegressor(n_estimators=50, min_samples_split=5,
+                               min_samples_leaf=2, criterion='mse')
 
 
 def handler(signum, frame):
@@ -135,9 +147,26 @@ if __name__ == '__main__':
     others.add_argument('-s', '--seeds', type=int,
         nargs=2, default=[None, None], metavar='SEED',
         help='specify the random seeds to be used (gym.env, np.random)')
+
+    log = others.add_mutually_exclusive_group()
+    log.add_argument('--log-level', metavar='LEVEL', default='INFO',
+        choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
+        help='Set loglevel to LEVEL')
+    log.add_argument('-v', '--verbose', action='store_true',
+        help='Show more output. Same as --log-level DEBUG')
+    log.add_argument('--quiet', action='store_true',
+        help='Show less output. Same as --log-level ERROR')
+
     others.add_argument('--help', action='help',
         help='show this help message and exit')
     args = parser.parse_args()
+
+    if args.verbose:
+        args.log_level = logging.DEBUG
+    elif args.quiet:
+        args.log_level = logging.ERROR
+    else:
+        args.log_level = getattr(logging, args.log_level)
 
     logging.config.dictConfig({
         'version': 1,
@@ -149,7 +178,7 @@ if __name__ == '__main__':
         'handlers': {
             'console': {
                 'class': 'logging.StreamHandler',
-                'level': logging.INFO,
+                'level': args.log_level,
                 'formatter': 'default',
             },
         },
@@ -159,7 +188,7 @@ if __name__ == '__main__':
             },
         },
         'root': {
-            'level': logging.INFO,
+            'level': logging.DEBUG,
             'handlers': ['console'],
         },
 
