e7c3bbc293c3b31d56df81e13aa2567322f6019d
Germano Gabbianelli <tyrion.mx@gmail.com>
2017-03-29 00:13:24 +0200

    Improve postprocess.py
    Correctly handle bool parameters.

--- experiments/postprocess.py
+++ experiments/postprocess.py
@@ -13,23 +13,24 @@ from trl import utils
 
 pat_fqi = r'n(\d\d)'
 pat_pbo = r'n(\d\d)K(\d\d)inc(\d)B(\d\d)l(\d\.\d\d)b(\d\d\d)no(inf|\d\d\d)'
-pat_gradpbo = r'n(\d\d)K(\d\d)inc(\d)ind0ui(\d\d)us\d\db(\d\d\d?)no(inf|\d\d\d)'
+pat_gradpbo = r'n(\d{2,3})K(\d\d)inc(\d)ind0ui(\d\d)us(\d\d)b(\d\d\d?)no(inf|\d{3})ul(None|auto|be)'
 
 f = float
 
-dtype_fqi = [('n', int), ('performance', f), ('time', f),
-    ('p_conf', f), ('t_conf', f)]
+dtype_fqi = np.dtype([('n', int), ('performance', f), ('time', f),
+    ('p_conf', f), ('t_conf', f)])
 
-dtype_pbo = [
+dtype_pbo = np.dtype([
     ('n', int), ('K', int), ('incremental', bool), ('budget', int),
     ('learning_rate', f),
     ('batch_size', int), ('norm', f), ('performance', f), ('time', f),
-    ('p_conf', f), ('t_conf', f)]
+    ('p_conf', f), ('t_conf', f)])
 
-dtype_gradpbo = [
+dtype_gradpbo = np.dtype([
     ('n', int), ('K', int), ('incremental', bool), ('update_index', int),
-    ('batch_size', int), ('norm', f), ('performance', f), ('time', f),
-    ('p_conf', f), ('t_conf', f)]
+    ('update_steps', int),
+    ('batch_size', int), ('norm', f), ('update_loss', '<S4'),
+    ('performance', f), ('time', f), ('p_conf', f), ('t_conf', f)])
 
 
 def average_experiment_runs(experiment_path):
@@ -46,17 +47,26 @@ def average_experiment_runs(experiment_path):
     conf = 1.96 * data.std(axis=0) / np.sqrt(n)
     return np.array([mean, conf]).ravel()
 
+
 def postprocess(base, pat, dtype):
     experiments = os.listdir(base)
     data = np.recarray((len(experiments),), dtype)
-    for i, name in enumerate(experiments):
+    i = 0
+    for name in experiments:
         m = re.match(pat, name)
         if m:
-            row = [t(x) for x, (_, t) in zip(m.groups(), dtype)]
+            row = []
+            for match, field in zip(m.groups(), dtype.names):
+                type = dtype.fields[field][0].type
+                match = int(match) if type is np.bool_ else match
+                row.append(type(match))
 
             path = os.path.join(base, name)
             row.extend(average_experiment_runs(path))
             data[i] = tuple(row)
+            i += 1
+
+    data = data[:i]
 
     utils.save_dataset(data, 'results-{}.h5'.format(base))
 
